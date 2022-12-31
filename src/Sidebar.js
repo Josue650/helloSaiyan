a7577ff5f6fb8d44dda0bd0c38b9f25132600742
Josue <cuellarjosue3@gmail.com>
2022-12-30 18:55:24 -0800

    made the search bar and header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import App from './App';
 import "./Sidebar.css";
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { Avatar, IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from "@mui/icons-material/MoreVert"
+import  SearchOutlinedIcon  from '@mui/icons-material/SearchOutlined';
 
-function sidebar() {
+
+function Sidebar() {
   return (
     <div className='sidebar'>
-    <h1>I am a sidebar</h1>
+     <div className='sidebar__header'>
+        <Avatar src="https://dragonball.guru/wp-content/uploads/2021/01/goku-dragon-ball-guru.jpg" />
+        <div className="sidebar__headerRight">
+            <IconButton>
+                <DonutLargeIcon/>
+            </IconButton>
+            <IconButton>
+                <ChatIcon/>
+            </IconButton>
+            <IconButton>
+                <MoreVertIcon />
+            </IconButton>
+        </div>
+     </div>
+
+     <div className='sidebar__search'>
+        <div className='sidebar__searchContainer'>
+            <SearchOutlinedIcon/>
+            <input placeholder='Search or start new chat' type='text'/>
+        </div>
+     </div>
     </div>
   )
 }
 
-export default sidebar
+export default Sidebar
