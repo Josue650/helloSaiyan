96fa2105be7e69b0de8cd756ac226841b2c9293e
Josue <cuellarjosue3@gmail.com>
2022-12-30 19:21:27 -0800

    Added sidebar chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import { Avatar, IconButton } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from "@mui/icons-material/MoreVert"
 import  SearchOutlinedIcon  from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 
 function Sidebar() {
@@ -30,6 +31,13 @@ function Sidebar() {
             <SearchOutlinedIcon/>
             <input placeholder='Search or start new chat' type='text'/>
         </div>
+     </div>
+
+     <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+
      </div>
     </div>
   )
